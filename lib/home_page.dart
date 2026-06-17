84586b45542860a076b527ee0ed49d57c045c7c5
birmehto <birmehto@gmail.com>
2026-06-17 23:09:18 +0530

    Provider And Stateprovider

---
+++ lib/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_example/main.dart';
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  void onSubmit(WidgetRef ref, String value) {
+    ref.read(nameProvider.notifier).update((state) => value);
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final name = ref.watch(nameProvider) ?? '';
+    return Scaffold(
+      appBar: AppBar(title: Text('Provider Example')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: .center,
+          crossAxisAlignment: .center,
+          children: [
+            TextFormField(
+              onChanged: (value) => onSubmit(ref, value),
+              // onFieldSubmitted: (value) => onSubmit(ref, value),
+            ),
+            SizedBox(height: 20),
+            Center(child: Text(name)),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:riverpod_example/home_page.dart';
 
+// Providers
+
+// final nameProvider = Provider<String>((ref) => "Bir");
+
+final nameProvider = StateProvider<String?>((ref) => null);
 void main() {
-  runApp(const MainApp());
+  runApp(ProviderScope(child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +17,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: HomePage());
   }
 }
